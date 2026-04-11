9d6f17a40a8b9f9262ae9dbd72227f778a92e10e
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2026-04-11 11:34:06 +0530

    Update test.sh

--- 04-Autoscaling kubernetes/horizonatl pod autoscaling(HPA)/test.sh
+++ 04-Autoscaling kubernetes/horizonatl pod autoscaling(HPA)/test.sh
@@ -1,10 +1,23 @@
-for j in {1..10}; do
-  for i in {1..1000}; do
-    curl -s -o /dev/null http://<YOUR-LOADBALANCER-URL> &
-  done
-  wait
-done
+#!/bin/bash
 
+# Set your LoadBalancer URL here
+URL="http://<your-loadbalancer-url>"
+
+# Outer loop (repeat 10 times)
+for j in {1..10}; do
+  
+  # Inner loop (1000 requests in parallel)
+  for i in {1..1000}; do
+    
+    # Send request silently in background
+    curl -s -o /dev/null $URL &
+  
+  done
+
+  # Wait for all background jobs to finish
+  wait
+
+done
 
 # Update your load balance For example Like this
 # http://a0fbf831ca4a94936954790ef7a89bc4-104519041.ap-south-1.elb.amazonaws.com/
