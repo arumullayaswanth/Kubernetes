275956a1f9b893d36d3ce296b1e47aab6c769682
Arumulla Yaswanth Reddy <yaswanth.arumulla@gmail.com>
2026-04-15 16:59:43 +0530

    add OIDC provider for EKS cluster

--- 01.EKS-terraform/main.tf
+++ 01.EKS-terraform/main.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
 
@@ -321,6 +325,29 @@ resource "aws_eks_cluster" "eks" {
 
   tags = {
     Name = "eks-cluster"
+  }
+}
+
+############################
+# OIDC PROVIDER
+############################
+
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+
+  depends_on = [aws_eks_cluster.eks]
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+
+  depends_on = [aws_eks_cluster.eks]
+
+  tags = {
+    Name = "eks-oidc-provider"
   }
 }
 
